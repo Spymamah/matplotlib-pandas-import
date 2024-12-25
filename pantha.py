1400dab1ce85bfb2106a869f63a96ac14f6c31b6
Your NameSpymamah <ikechukwumamah1@gmail.com>
2024-12-25 14:14:26 +0100

    new

--- pantha.py
+++ pantha.py
@@ -21,10 +21,10 @@ breast_cancer_df = pd.DataFrame(breast_cancer.data, columns = breast_cancer.feat
 # now = breast_df.info()
 # print(now)
 print(breast_cancer) 
-breast_cancer_df['Price'] = breast_cancer.target
+# breast_cancer_df['Price'] = breast_cancer.target
 print(breast_cancer_df.head())
-now1 = breast_cancer_df.value_counts('Price')
-print(now1)
-now2 = breast_cancer_df.groupby('Price').mean()
-print(now2)
+# now1 = breast_cancer_df.value_counts('Price')
+# print(now1)
+# now2 = breast_cancer_df.groupby('Price').mean()
+# print(now2)
 print(breast_cancer_df.corr())
